90206ca9ab317435ec7ea3c9945407227952c758
graveetone <zastavskijs@gmail.com>
2023-11-11 15:35:36 +0200

    adds filtering for points

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -5,39 +5,58 @@ import Route from './Route'
 const MODE = 'walking'
 
 // вул. Левка Лук’яненка, 3, 3-В
-const testCenterPoint = locations.find(location => location.id === 30)
+// const testCenterPoint = locations[0]//.find(location => location.id)
 
 const CENTER = [
-    testCenterPoint.longitude,
-    testCenterPoint.latitude
+    30.48825808474481,
+    50.50826373625383
 ]
 
 
 const ZOOM_LEVEL = 13;
 
 const map = new MapBoxMap(CENTER, ZOOM_LEVEL);
+var startPoint = null;
+var endPoint = null;
+var points = [];
 
 map.mapObject.on('load', () => {
     locations.forEach(location => {
-        let point = new Point(location.id, location.address, location.longitude, location.latitude, 'blue', 8)
+        let point = new Point(location.id, location.address, location.longitude, location.latitude, 'green', 8)
         map.drawPoint(point)
     });
 
 
-    map.mapObject.on('click', async (event) => {
-        // const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
-        // const endPoint = new Point(
-        //     'endPoint', ...coords, 'black', 10
-        // )
-        // map.drawPoint(endPoint);
-
-        // const route = new Route(
-        //     'route', null,
-        //     'black', 2, 0.75
-        // )    
-
-        // await route.build([startPoint, middlePoint, endPoint], MODE)
-
-        // map.drawRoute(route);
+    map.mapObject.on('click', (event) => {
+        const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
+        startPoint = new Point(
+            'startPoint', 'Точка відправлення', ...coords, 'red', 10
+        )
+        map.drawPoint(startPoint);
     })
+
+    map.mapObject.on('contextmenu', async (event) => {
+        const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
+        endPoint = new Point(
+            'endPoint', 'Точка прибуття', ...coords, 'blue', 10
+        )
+        map.drawPoint(endPoint);
+
+        // points[points.length] = endPoint;
+        // alert(points)
+        const pointsForRoute = [startPoint, ...points, endPoint]
+        if (pointsForRoute.length >= 2) {
+
+            const route = new Route(
+                'route', null,
+                'black', 2, 0.75
+            )
+            
+            await route.build(pointsForRoute, MODE)
+
+            map.drawRoute(route);
+        }
+    })
+
 })
+
